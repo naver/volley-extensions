b806a8ea765f97f6a18933185f32ca5f903ded95
Wonjun Kim <wonjun.kim@navercorp.com>
2014-05-27 17:59:31 +0900

    [Issue-132] Add getRequestCreator() method into VolleyerContext

--- volleyer/src/main/java/com/navercorp/volleyextensions/volleyer/VolleyerContext.java
+++ volleyer/src/main/java/com/navercorp/volleyextensions/volleyer/VolleyerContext.java
@@ -1,4 +1,11 @@
 package com.navercorp.volleyextensions.volleyer;
 
+import com.navercorp.volleyextensions.volleyer.request.creator.DefaultRequestCreator;
+import com.navercorp.volleyextensions.volleyer.request.creator.RequestCreator;
+
 public class VolleyerContext {
+	private RequestCreator requestCreator = new DefaultRequestCreator();
+	public RequestCreator getRequestCreator() {
+		return requestCreator;
+	}
 }
