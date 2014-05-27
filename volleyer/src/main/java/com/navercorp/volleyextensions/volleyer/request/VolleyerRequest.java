23b2a1fdde62e188c7541677c23f0e1357de02a3
Wonjun Kim <wonjun.kim@navercorp.com>
2014-05-27 17:34:58 +0900

    [Issue-129] Add VolleyerRequest

---
+++ volleyer/src/main/java/com/navercorp/volleyextensions/volleyer/request/VolleyerRequest.java
@@ -0,0 +1,60 @@
+package com.navercorp.volleyextensions.volleyer.request;
+
+import java.util.Map;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.Response.ErrorListener;
+import com.android.volley.Response.Listener;
+import com.navercorp.volleyextensions.volleyer.http.HttpContent;
+import com.navercorp.volleyextensions.volleyer.response.parser.NetworkResponseParser;
+import com.navercorp.volleyextensions.volleyer.util.Assert;
+
+public class VolleyerRequest<T> extends Request<T> {
+
+	private NetworkResponseParser responseParser;
+	private Listener<T> listener;
+	private Class<T> clazz;
+	private HttpContent httpContent;
+
+	public VolleyerRequest(HttpContent httpContent, Class<T> clazz, NetworkResponseParser responseParser, Listener<T> listener, ErrorListener errorListener) {
+		super(httpContent.getMethod().getMethodCode(), httpContent.getUrl(), errorListener);
+
+		Assert.notNull(clazz, "Target class token");
+		Assert.notNull(responseParser, "NetworkResponseParser");
+		Assert.notNull(listener, "listener");
+
+		this.httpContent = httpContent;
+		this.responseParser = responseParser;
+		this.listener = listener;
+		this.clazz = clazz;
+		
+	}
+
+	@Override
+	public Map<String, String> getHeaders() throws AuthFailureError {
+		return httpContent.getHeaders();
+	}
+
+	@Override
+	public byte[] getBody() {
+		return httpContent.getBody();
+	}
+
+	@Override
+	protected void deliverResponse(T result) {
+		listener.onResponse(result);
+	}
+	/**
+	 * @return Specific type object of an converted object from response data
+	 */
+	protected Class<T> getTargetClass() {
+		return this.clazz;
+	}
+
+	protected Response<T> parseNetworkResponse(NetworkResponse response) {
+		return responseParser.parseNetworkResponse(response, clazz);
+	}
+}
