0fcc12f4ca54f46465f9c4fe931a4eeab79a821d
Wonjun Kim <wonjun.kim@navercorp.com>
2014-05-28 14:53:59 +0900

    [Issue-136] Add getRequestExecutor() in VolleyerContext

--- volleyer/src/main/java/com/navercorp/volleyextensions/volleyer/VolleyerContext.java
+++ volleyer/src/main/java/com/navercorp/volleyextensions/volleyer/VolleyerContext.java
@@ -2,10 +2,17 @@ package com.navercorp.volleyextensions.volleyer;
 
 import com.navercorp.volleyextensions.volleyer.request.creator.DefaultRequestCreator;
 import com.navercorp.volleyextensions.volleyer.request.creator.RequestCreator;
+import com.navercorp.volleyextensions.volleyer.request.executor.DefaultRequestExecutor;
+import com.navercorp.volleyextensions.volleyer.request.executor.RequestExecutor;
 
 public class VolleyerContext {
 	private RequestCreator requestCreator = new DefaultRequestCreator();
+	private RequestExecutor requestExecutor = new DefaultRequestExecutor();
 	public RequestCreator getRequestCreator() {
 		return requestCreator;
+	}
+
+	public RequestExecutor getRequestExecutor() {
+		return requestExecutor;
 	}
 }
