a03878d0bdc80cb6156a70de79df982b9f0e8881
Wonjun Kim <wonjun.kim@navercorp.com>
2014-05-29 18:40:13 +0900

    [Issue-139] Add VolleyContext.getDefaultNetworkResponseParser() method

--- volleyer/src/main/java/com/navercorp/volleyextensions/volleyer/VolleyerContext.java
+++ volleyer/src/main/java/com/navercorp/volleyextensions/volleyer/VolleyerContext.java
@@ -4,15 +4,27 @@ import com.navercorp.volleyextensions.volleyer.request.creator.DefaultRequestCre
 import com.navercorp.volleyextensions.volleyer.request.creator.RequestCreator;
 import com.navercorp.volleyextensions.volleyer.request.executor.DefaultRequestExecutor;
 import com.navercorp.volleyextensions.volleyer.request.executor.RequestExecutor;
+import com.navercorp.volleyextensions.volleyer.response.parser.IntegratedNetworkResponseParserBuilder;
+import com.navercorp.volleyextensions.volleyer.response.parser.Jackson2NetworkResponseParser;
+import com.navercorp.volleyextensions.volleyer.response.parser.NetworkResponseParser;
+import com.navercorp.volleyextensions.volleyer.response.parser.SimpleXmlNetworkResponseParser;
 
 public class VolleyerContext {
 	private RequestCreator requestCreator = new DefaultRequestCreator();
 	private RequestExecutor requestExecutor = new DefaultRequestExecutor();
+	private NetworkResponseParser defaultNetworkResponseParser = new IntegratedNetworkResponseParserBuilder()
+																	.addParser(new Jackson2NetworkResponseParser())
+																	.addParser(new SimpleXmlNetworkResponseParser())
+																	.build(); 
 	public RequestCreator getRequestCreator() {
 		return requestCreator;
 	}
 
 	public RequestExecutor getRequestExecutor() {
 		return requestExecutor;
+	}
+
+	public NetworkResponseParser getDefaultNetworkResponseParser() {
+		return defaultNetworkResponseParser;
 	}
 }
