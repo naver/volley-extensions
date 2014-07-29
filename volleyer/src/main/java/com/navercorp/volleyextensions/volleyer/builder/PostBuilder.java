e6bd72a0f4d94804be0435c4eb1e8e14a94d6199
Wonjun Kim <wonjun.kim@navercorp.com>
2014-07-29 14:53:01 +0900

    [Issue-186] Add body/multipart support for PutBuilder
    To support, sevaral methods moves from PostBuilder to new abstract class BodyBuilder,
    and PostBuilder, PutBuilder override it.

--- volleyer/src/main/java/com/navercorp/volleyextensions/volleyer/builder/PostBuilder.java
+++ volleyer/src/main/java/com/navercorp/volleyextensions/volleyer/builder/PostBuilder.java
@@ -1,66 +1,14 @@
 package com.navercorp.volleyextensions.volleyer.builder;
 
-import java.io.File;
 
 import com.android.volley.RequestQueue;
 import com.navercorp.volleyextensions.volleyer.VolleyerConfiguration;
 import com.navercorp.volleyextensions.volleyer.http.HttpMethod;
-import com.navercorp.volleyextensions.volleyer.multipart.FilePart;
-import com.navercorp.volleyextensions.volleyer.multipart.Part;
-import com.navercorp.volleyextensions.volleyer.multipart.StringPart;
-import com.navercorp.volleyextensions.volleyer.util.VolleyerLog;
 
-public class PostBuilder extends RequestBuilder<PostBuilder> {
+public class PostBuilder extends BodyBuilder<PostBuilder> {
 
 	public PostBuilder(RequestQueue requestQueue, VolleyerConfiguration configuration, String url) {
 		super(requestQueue, configuration, url, HttpMethod.POST);
 	}
 
-	public PostBuilder setBody(byte[] body) {
-		if (isBodyNull(body)) {
-			return this;
-		}
-
-		httpContent.setBody(body);
-		return this;
-	}
-
-	public PostBuilder setBody(String body) {
-		if (isBodyNull(body)) {
-			return this;
-		}
-
-		byte[] bytes = body.getBytes();
-		return setBody(bytes);
-	}
-
-	private boolean isBodyNull(Object body) {
-		boolean isNull = false;
-
-		if (body == null) {
-			VolleyerLog.warn("You have to set the body which is not null.");
-			isNull = true;
-		}
-
-		return isNull;
-	}
-
-	public PostBuilder addFilePart(File file) {
-		return addFilePart(file.getName(), file);
-	}
-
-	public PostBuilder addFilePart(String name, File file) {
-		httpContent.addPart(new FilePart(name, file));
-		return this;
-	}
-
-	public PostBuilder addStringPart(String name, String value) {
-		httpContent.addPart(new StringPart(name, value));
-		return this;
-	}
-
-	public PostBuilder addPart(Part part) {
-		httpContent.addPart(part);
-		return this;
-	}
 }
--- volleyer/src/main/java/com/navercorp/volleyextensions/volleyer/builder/PutBuilder.java
+++ volleyer/src/main/java/com/navercorp/volleyextensions/volleyer/builder/PutBuilder.java
@@ -4,7 +4,7 @@ import com.android.volley.RequestQueue;
 import com.navercorp.volleyextensions.volleyer.VolleyerConfiguration;
 import com.navercorp.volleyextensions.volleyer.http.HttpMethod;
 
-public class PutBuilder extends RequestBuilder<PutBuilder> {
+public class PutBuilder extends BodyBuilder<PutBuilder> {
 
 	public PutBuilder(RequestQueue requestQueue, VolleyerConfiguration configuration, String url) {
 		super(requestQueue, configuration, url, HttpMethod.PUT);
